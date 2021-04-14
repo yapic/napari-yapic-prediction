7a719b7b10a8c6184d23517d3c4c03fb71aad905
dlesmesl <dlesmesleon@hotmail.com>
2021-04-14 16:10:58 +0200

    .

--- napari_yapic_prediction/_dock_widget.py
+++ napari_yapic_prediction/_dock_widget.py
@@ -1,48 +1,48 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
+# """
+# This module is an example of a barebones QWidget plugin for napari
 
-It implements the ``napari_experimental_provide_dock_widget`` hook specification.
-see: https://napari.org/docs/dev/plugins/hook_specifications.html
+# It implements the ``napari_experimental_provide_dock_widget`` hook specification.
+# see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
-Replace code below according to your needs.
-"""
-from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
-
-
-class MyWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+# Replace code below according to your needs.
+# """
+# from napari_plugin_engine import napari_hook_implementation
+# from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 
 
+# class MyWidget(QWidget):
+#     # your QWidget.__init__ can optionally request the napari viewer instance
+#     # in one of two ways:
+#     # 1. use a parameter called `napari_viewer`, as done here
+#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+#     def __init__(self, napari_viewer):
+#         super().__init__()
+#         self.viewer = napari_viewer
 
-@napari_hook_implementation
-def napari_experimental_provide_dock_widget():
-    return MyWidget
+#         btn = QPushButton("Click me!")
+#         btn.clicked.connect(self._on_click)
+
+#         self.setLayout(QHBoxLayout())
+#         self.layout().addWidget(btn)
+
+#     def _on_click(self):
+#         print("napari has", len(self.viewer.layers), "layers")
+
+
+
+# @napari_hook_implementation
+# def napari_experimental_provide_dock_widget():
+#     return MyWidget
 
 # _________________________________________
 
-# from napari_plugin_engine import napari_hook_implementation
-# from magicgui.widgets import FileEdit, Label, Container, ProgressBar, PushButton
-# from napari_yapic_prediction.yapic_dependencies.yapic_prediction import yapic_prediction
-# from magicgui import magicgui, magic_factory
-# from pathlib import Path
-# from napari import Viewer
-# import napari
+from napari_plugin_engine import napari_hook_implementation
+from magicgui.widgets import FileEdit, Label, Container, ProgressBar, PushButton
+from napari_yapic_prediction.yapic_dependencies.yapic_prediction import yapic_prediction
+from magicgui import magicgui, magic_factory
+from pathlib import Path
+from napari import Viewer
+import napari
 
 # def mywidget(napari_viewer: napari.viewer.Viewer):
 #     # make some widgets
@@ -72,3 +72,14 @@ def napari_experimental_provide_dock_widget():
 #     return mywidget
 
 # _________________________________________________________________
+
+@magicgui(call_button="Predict", layout="vertical",
+          filename={"label": "Select a model:"},
+          progress={'widget_type': "ProgressBar", 'label': 'Prediction mapping:', 'visible': True, 'min': 0, 'max': 1})
+def test_func(napari_viewer: Viewer, filename=Path.home(), progress: ProgressBar = 0):
+    yapic_prediction(filename, napari_viewer, None)
+    return None
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return test_func
