e8f1c29bbae8eac35489c55d186d10254a64fb76
dlesmesl <dlesmesleon@hotmail.com>
2021-04-14 17:21:17 +0200

    .

--- napari_yapic_prediction/_dock_widget.py
+++ napari_yapic_prediction/_dock_widget.py
@@ -1,75 +1,72 @@
-# """
-# This module is an example of a barebones QWidget plugin for napari
+"""
+This module is an example of a barebones QWidget plugin for napari
 
-# It implements the ``napari_experimental_provide_dock_widget`` hook specification.
-# see: https://napari.org/docs/dev/plugins/hook_specifications.html
+It implements the ``napari_experimental_provide_dock_widget`` hook specification.
+see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
-# Replace code below according to your needs.
-# """
-# from napari_plugin_engine import napari_hook_implementation
-# from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
-
-
-# class MyWidget(QWidget):
-#     # your QWidget.__init__ can optionally request the napari viewer instance
-#     # in one of two ways:
-#     # 1. use a parameter called `napari_viewer`, as done here
-#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-#     def __init__(self, napari_viewer):
-#         super().__init__()
-#         self.viewer = napari_viewer
-
-#         btn = QPushButton("Click me!")
-#         btn.clicked.connect(self._on_click)
-
-#         self.setLayout(QHBoxLayout())
-#         self.layout().addWidget(btn)
-
-#     def _on_click(self):
-#         print("napari has", len(self.viewer.layers), "layers")
-
-
-
-# @napari_hook_implementation
-# def napari_experimental_provide_dock_widget():
-#     return MyWidget
-
-# _________________________________________
-
+Replace code below according to your needs.
+"""
+from qtpy.QtWidgets import QWidget
 from napari_plugin_engine import napari_hook_implementation
-from magicgui.widgets import FileEdit, Label, Container, ProgressBar, PushButton
-from napari_yapic_prediction.yapic_dependencies.yapic_prediction import yapic_prediction
-from magicgui import magicgui, magic_factory
-from pathlib import Path
-from napari import Viewer
-import napari
 
-def mywidget(napari_viewer: napari.viewer.Viewer):
-    # make some widgets
-    file_picker = FileEdit(label='Model file path:', value='')
-    label = Label(label='Uploaded model:', value=file_picker.value)
-    button = PushButton(label='Predict')
-    progress = ProgressBar(label='Prediction mapping:', visible=False, value = 0, min = 0, max=1)
+class MyWidget(QWidget):
+    def __init__(self, napari_viewer):
+        self.viewer = napari_viewer
+        super().__init__()
 
-    #set up callbacks
-    def set_label(event):
-        label.value = file_picker.value.name
+        # initialize layout
+        layout = QGridLayout()
 
-    def prediction(event):
-        progress.visible = True
-        yapic_prediction(file_picker.value, napari_viewer, progress)
-            
+        # add a button
+        btn = QPushButton('Click me!', self)
+        def trigger():
+            print("napari has", len(napari_viewer.layers), "layers")
+        btn.clicked.connect(trigger)
+        layout.addWidget(btn)
 
-    file_picker.changed.connect(set_label)
-    button.changed.connect(prediction)
-
-    # create a container to hold the widgets:
-    container = Container(widgets=[file_picker, label, button, progress])
-    return container, {'area':'left'}
+        # activate layout
+        self.setLayout(layout)
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return mywidget, {'napari_viewer': napari.viewer.Viewer}
+    return MyWidget
+
+# _________________________________________
+
+# from napari_plugin_engine import napari_hook_implementation
+# from magicgui.widgets import FileEdit, Label, Container, ProgressBar, PushButton
+# from napari_yapic_prediction.yapic_dependencies.yapic_prediction import yapic_prediction
+# from magicgui import magicgui, magic_factory
+# from pathlib import Path
+# from napari import Viewer
+# import napari
+
+# def mywidget(napari_viewer: napari.viewer.Viewer):
+#     # make some widgets
+#     file_picker = FileEdit(label='Model file path:', value='')
+#     label = Label(label='Uploaded model:', value=file_picker.value)
+#     button = PushButton(label='Predict')
+#     progress = ProgressBar(label='Prediction mapping:', visible=False, value = 0, min = 0, max=1)
+
+#     #set up callbacks
+#     def set_label(event):
+#         label.value = file_picker.value.name
+
+#     def prediction(event):
+#         progress.visible = True
+#         yapic_prediction(file_picker.value, napari_viewer, progress)
+            
+
+#     file_picker.changed.connect(set_label)
+#     button.changed.connect(prediction)
+
+#     # create a container to hold the widgets:
+#     container = Container(widgets=[file_picker, label, button, progress])
+#     return container, {'area':'left'}
+
+# @napari_hook_implementation
+# def napari_experimental_provide_dock_widget():
+#     return mywidget
 
 # _________________________________________________________________
 
